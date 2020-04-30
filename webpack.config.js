8416709cd88cb6a9bcbbdd36b5bd6520baab6c11
Elias Kuiter <info@elias-kuiter.de>
2020-04-30 18:20:01 +0200

    Proper cache invalidation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "production",
     entry: "./app/js/index.js",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "",
+            filename: "../index.html"
+        }),
+    ],
     output: {
         path: path.resolve(__dirname, "app", "dist"),
-        publicPath: "/dist/"
+        publicPath: "/dist/",
+        filename: "[name].[contenthash].js"
     },
     module: {
         rules: [
